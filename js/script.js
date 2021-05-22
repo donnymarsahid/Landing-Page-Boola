113283e3e0a3a998365afcbbd047e23e27bfd05c
unknown <donnymrshd.94@gmail.com>
2021-05-22 00:59:56 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// Parralax
+
+$(window).scroll(function () {
+  let wScroll = $(this).scrollTop();
+
+  // textFeatures
+  if (wScroll > 292) {
+    $('.features .textFeatures').addClass('munculTextFeatures');
+  }
+  // Features
+  if (wScroll > 297) {
+    $('.features .kotak').addClass('munculFeatures');
+  }
+  // Download
+  if (wScroll > $('.container .download').offset().top - 620) {
+    $('.container .download').addClass('munculDownload');
+  }
+  // CaraKerja
+  if (wScroll > $('.caraKerjaText').offset().top - 620) {
+    $('.caraKerjaText').addClass('munculCaraKerjaText');
+  }
+  if (wScroll > $('.caraKerjaContent').offset().top - 970) {
+    $('.caraKerjaContent').addClass('munculCaraKerjaContent');
+  }
+  // gabung
+  if (wScroll > $('.gabung').offset().top - 1700) {
+    $('.gabung').addClass('munculGabung');
+  }
+});
+
+// Header {
+$('header .headerText').addClass('munculText');
+$('header .headerImage').addClass('munculImage');
+
+// PageScroll
+$('.page-scroll').on('click', function (e) {
+  let href = $(this).attr('href');
+  let elemenHref = $(href);
+
+  $('html, body').animate(
+    {
+      scrollTop: elemenHref.offset().top - 50,
+    },
+    500,
+    'easeInOutExpo'
+  );
+
+  e.preventDevault();
+});
